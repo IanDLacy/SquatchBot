a3643c2ad8bc85ffea6d6c8f53a55e807d2ba1cf
IanDLacy <Ian.D.Lacy@Gmail.com>
2020-09-14 20:50:34 -0400

    Customization & Clean Up
    - Finished Customizing Command "Help"
    - Cleaned Up Module "Bot"
    - Cleaned Up Command "Terminate"

--- SquatchBot/extensions/terminate.py
+++ SquatchBot/extensions/terminate.py
@@ -1,10 +1,12 @@
 from discord.ext import commands
 
 @commands.command	(
+
 			name = 'Terminate' ,
 			brief = 'Terminate SquatchBot' ,
 			description = 'Terminate SquatchBot' ,
 			aliases =	[
+
 					'close'  ,
 					'die'    ,
 					'end'    ,
@@ -13,29 +15,27 @@ from discord.ext import commands
 					'leave'  ,
 					'logout' ,
 					'quit'
+
 					] ,
 			usage = '[ No Arguments ]' ,
 			help =	'Running this command causes the SquatchBot process to disconnect and end.' \
 				'\nIf SquatchBot is running from a daemon, it will restart shortly.' ,
-			# enabled = True ,
-			# parent = [ Command ] ,
-			# cog = [ Cog ] ,
-			# hidden = True ,
-			# rest_is_raw = [ bool (False) ] ,
-			# require_var_positional = [ bool (false) ] ,
-			# ignore_extra = [ bool (True) ] ,
-			# cooldown_after_parsing = [ bool (False) ] ,
-			# cog_name = [ str ] ,
+			enabled = True ,
+			hidden = False ,
+			rest_is_raw = False ,
+			require_var_positional = False ,
+			ignore_extra = True ,
+			cooldown_after_parsing = False
+
 			)
-async def terminate (context) :
-	"""Equivalent To [brief=[str]], But Gets Overridden"""
+async def terminate ( context ) :
 	await context.send ( '```fix\n> Exit```' )
-	await context.bot.logout()
+	await context.bot.logout (  )
 	print ( '\nSquatchBot : Exit\n' )
 
-def setup(bot) :
-	bot.add_command(terminate)
-	print('\nSquatchBot : Extension Setup [ Terminate ]')
+def setup ( bot ) :
+	bot.add_command ( terminate )
+	print ( '\nSquatchBot : Extension Setup [ Terminate ]' )
 
-def teardown(bot) :
-	print('\nSquatchBot : Extension Teardown [ Terminate ]')
+def teardown ( bot ) :
+	print ( '\nSquatchBot : Extension Teardown [ Terminate ]')
--- SquatchBot/modules/bot.py
+++ SquatchBot/modules/bot.py
@@ -1,115 +1,87 @@
 from modules.token import token
 
-print ('\nSquatchBot : Starting')
+print ( '\nSquatchBot : Starting' )
 
 from discord.ext import commands
 
-commands.DefaultHelpCommand.get_ending_note = \
-	lambda self : \
-		f'Run `{self.clean_prefix}{self.invoked_with} [ Category | Command ]` for specifics.\n'
+commands.DefaultHelpCommand.get_ending_note = lambda self : \
+	f'Run `{self.clean_prefix}{self.invoked_with} [ Category | Command ]` for specifics.\n'
 
-bot = commands.Bot (
-	# command_prefix =  [ Callable ( func | coro ) ] | [ Iterable [ str ] ] | [ str ]
-	command_prefix = commands.when_mentioned_or (
+bot = commands.Bot	(
 
-		'sb> ' ,
-		'Sb> ' ,
-		'SB> ' ,
+			command_prefix = commands.when_mentioned_or	(
 
-		'sb>' ,
-		'Sb>' ,
-		'SB>' ,
+									'sb> ' , 'Sb> ' , 'SB> ' ,
+									'sb>'  , 'Sb>'  , 'SB>'  ,
 
-		'sb://' ,
-		'Sb://' ,
-		'SB://' ,
+									'sb://' , 'Sb://' , 'SB://' ,
 
-		'sb: ' ,
-		'Sb: ' ,
-		'SB: ' ,
+									'sb: ' , 'Sb: ' , 'SB: ' ,
+									'sb:'  , 'Sb:'  , 'SB:'  ,
 
-		'sb:' ,
-		'Sb:' ,
-		'SB:' ,
+									'sb$ ' , 'Sb$ ' , 'SB$ ' ,
+									'sb$'  , 'Sb$'  , 'SB$'  ,
 
-		'sb$ ' ,
-		'Sb$ ' ,
-		'SB$ ' ,
+									'sb# ' , 'Sb# ' , 'SB# ' ,
+									'sb#'  , 'Sb#'  , 'SB#'  ,
 
-		'sb$' ,
-		'Sb$' ,
-		'SB$' ,
+									'sb.' , 'Sb.' , 'SB.' ,
+									'sb/' , 'Sb/' , 'SB/' ,
+									'sb?' , 'Sb?' , 'SB?' ,
 
-		'sb# ' ,
-		'Sb# ' ,
-		'SB# ' ,
+									'sb ' , 'Sb ' , 'SB ' ,
+									'sb'  , 'Sb'  , 'SB'  ,
 
-		'sb#' ,
-		'Sb#' ,
-		'SB#' ,
+									'squatchbot ' , 'Squatchbot ' , 'SquatchBot ' , 'SQUATCHBOT '
 
-		'sb.' ,
-		'Sb.' ,
-		'SB.' ,
+									) ,
+			case_insensitive = True ,
+			description = 'SquatchBot!' ,
+			self_bot = False ,
+			help_command = commands.DefaultHelpCommand	(
 
-		'sb/' ,
-		'Sb/' ,
-		'SB/' ,
+									width = 128 ,
+									sort_commands = True ,
+									dm_help = False ,
+									dm_help_threshold = 1000 ,
+									indent = 2 ,
+									commands_heading = 'Commands:' ,
+									command_attrs =	{
 
-		'sb?' ,
-		'Sb?' ,
-		'SB?' ,
+											'name' : 'Help' ,
+											'brief' : 'SquatchBot Help' ,
+											'description' : 'SquatchBot Help' ,
+											'aliases' :	[
 
-		'sb ' ,
-		'Sb ' ,
-		'SB ' ,
+													'?'      ,
+													'-h'     ,
+													'--help' ,
+													'h'
 
-		'sb' ,
-		'Sb' ,
-		'SB' ,
+													] ,
+											'usage' : '[ Category | Command ]' ,
+											'help' : 'Running this command displays various help information.'
 
-		'squatchbot ' ,
-		'Squatchbot ' ,
-		'SquatchBot ' ,
-		'SQUATCHBOT '
-	) ,
-	case_insensitive = True ,
-	description = 'SquatchBot!' ,
-	self_bot = False ,
-	help_command = commands.DefaultHelpCommand	(
-							width = 128 ,
-							# sort_commands = True ,
-							# dm_help = [ True | False | None ] ,
-							# dm_help_threshold = 1000 ,
-							# indent = 2 ,
-							# commands_heading = 'Commands:' ,
-							command_attrs =	{
-									'name' : 'Help' ,
-									'brief' : 'SquatchBot Help' ,
-									'description' : 'SquatchBot Help' ,
-									'aliases' :	[
-											'?'      ,
-											'-h'     ,
-											'--help' ,
-											'h'
-											] ,
-									'usage' : '[ Category | Command ]' ,
-									'help' : 'Running this command displays various help information.' ,
-									} ,
-							no_category = 'SquatchBot' ,
-							# paginator = [ Paginator ] ,
-							) ,
-	# owner_id = [ int ] | owner_ids = [ Collection ( set ) [ int ] ] ,
-	# activity = [ BaseActivity ] ,
-	# allowed_mentions = [ AllowedMentions ] ,
-	# user = [ ClientUser ] ,
-)
+											} ,
+									no_category = 'Miscellaneous' ,
+									paginator = commands.Paginator	(
+
+													prefix = '```fix' ,
+													suffix = '```' ,
+													max_size = 2000
+
+													)
+
+									) ,
+
+			)
 
 import os
 
 for file in os.listdir( './SquatchBot/extensions' ) :
 	if file.endswith('.py') :
 		bot.load_extension(f'extensions.{file[:-3]}')
+
 @bot.event
 async def on_ready():
 	print('\nSquatchBot : Ready')
